48c27a1f056824237f5638446996423da2bfdcbe
guruprakash-c <c.guruprakash@yahoo.com>
2025-05-31 18:29:58 +0530

    Computed Properties

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,11 +2,14 @@
 import Sidebar from './Sidebar.vue';
 import MyReactiveComponents from './concepts/MyReactiveComponents.vue'
 import MyRefComponents from './concepts/MyRefComponents.vue'
+import ComputedProperties from './concepts/ComputedProperties.vue'
+
 </script>
 <template>
     <main class="col-lg-8 col-md-8 col-xs-12  col-sm-12">
         <!-- MyReactiveComponents/ -->
-        <MyRefComponents/>
+        <!-- MyRefComponents/ -->
+        <ComputedProperties/>
     </main>
     <Sidebar/>
 </template>
---
+++ src/components/concepts/ComputedProperties.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref, computed } from 'vue';
+const number = ref(1);
+const squaredNumber = computed(() => number.value * number.value);
+</script>
+<template>
+    <article>
+        <h1>Computed Properties </h1>
+        <pre>
+        Original number: {{ number }}
+        Squared number: {{ squaredNumber }}
+        </pre>
+        <input type="number" class="form-control w-25" @change="number = inputValue" v-model="inputValue" min="0" step="2" max="10"/>
+    </article>
+</template>
